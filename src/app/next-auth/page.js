c96624f3f25e4115cfc07ad8c3a57d86526ec575
AnkurNayak <ankurzac3@gmail.com>
2024-12-26 12:51:33 +0530

    next-auth_implementation_11:testing

--- src/app/next-auth/page.js
+++ src/app/next-auth/page.js
@@ -3,12 +3,6 @@ import ClientSideLoginComponent from "@/components/next-auth/ClientSideLoginComp
 import { SessionProvider } from "next-auth/react";
 
 const NextAuthComponentPage = () => {
-  console.log(process.env.NEXTAUTH_URL);
-  console.log(process.env.NEXTAUTH_SECRET);
-  console.log(process.env.GITHUB_CLIENT_ID);
-  console.log(process.env.GITHUB_CLIENT_SECRET);
-  console.log(process.env.GOOGLE_CLIENT_ID);
-  console.log(process.env.GOOGLE_CLIENT_SECRET);
   return (
     <SessionProvider>
       <ClientSideLoginComponent />
